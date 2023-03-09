abafa21ea5318cdcb27e5a34057fb8c79d217841
Diego <diegocristianodospassos@gmail.com>
2023-03-09 10:11:46 -0300

    s

--- node-project/server.js
+++ node-project/server.js
@@ -4,8 +4,8 @@ const redis = require('redis');
 
 const app = express();
 const client = redis.createClient({
-  host: '10.102.113.150',
-  port: 30001
+  host: 'redis',
+  port: 6379
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
